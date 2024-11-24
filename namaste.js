5c2247ee0a7a4677534e9b22de19597f048ded26
aniket kushwaha <150453453+aniketkush@users.noreply.github.com>
2024-11-25 02:23:58 +0530

    Add files via upload

---
+++ namaste.js
@@ -0,0 +1,9 @@
+// Function to greet a person
+function greetPerson(firstName, lastName) {
+    console.log(`Hello, ${firstName} ${lastName}! Welcome!`);
+}
+
+// Example usage
+const firstName = prompt("Enter your first name:");
+const lastName = prompt("Enter your last name:");
+greetPerson(firstName, lastName);
